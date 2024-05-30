6ef37d3f0a673c1a3585fd7ca3d064d485b38f03
kirill pavlov <2020kirillp@gmail.com>
2024-05-30 12:41:24 +0100

    Config updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	output: "export", // <=== enables static exports
+	basePath: "/neet-waitlist",
+	reactStrictMode: true,
+};
 
 export default nextConfig;
